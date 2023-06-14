93a966bc0249d3d816d31793578f9d919eccb725
RehmaWarsi <112500083+RehmaWarsi@users.noreply.github.com>
2023-06-14 17:57:29 +0500

    Update test.php

--- CS/test.php
+++ CS/test.php
@@ -1,15 +1,3 @@
 <?php
 echo "hello";
 ?>
-
-can you send this folder to me ill try on my laptop i think there is a problem in the xampp configuration
-listen
-
-Pora ek bar bana lu phir send krdu
-ta kai mera project at least 80 percent complete ho jaye?
-
-can you wait just a little?
-
-i can , but you can keep doing it atleast ye to pata chale problem kahan hai
-
-ok get it, than you will explain me if you dignose it? yea ok ok i am sending it to you 
